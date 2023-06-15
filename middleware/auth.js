58695ea8e969bb45e794c1bc581d81a905855c94
Mitali Padiya <mdpadiya10@gmail.com>
2023-06-15 17:50:18 +0530

    Fixed reset password

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,12 +5,14 @@ const config = process.env;
 const verifyToken = (req, res, next) => {
   const token =
     req.body.token || req.query.token || req.headers.authorization;
+    console.log("token ==>", token);
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
   }
   try {
     const decoded = jwt.verify(token, config.TOKEN_KEY);
+    console.log("decoded user ==>", decoded);
     req.user = decoded;
   } catch (err) {
     console.log(err);
